ce374042481f4102fc7944c0d3a158710427b745
Isaac Cummings <Cummingsi1993@gmail.com>
2015-06-30 20:54:23 -0400

    more diagnostics

--- deploy.cmd
+++ deploy.cmd
@@ -99,10 +99,12 @@ call :SelectNodeVersion
 
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+echo installing node packages.
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
+  echo Finished installing packages
 )
 
 :: 4. Create Hubot file with a coffee extension
