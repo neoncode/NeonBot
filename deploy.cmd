feb8590d44b7978fde5fd875cd5b7c708dbab93a
Isaac Cummings <Cummingsi1993@gmail.com>
2015-06-30 20:48:36 -0400

    add some diagnostics

--- deploy.cmd
+++ deploy.cmd
@@ -106,6 +106,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 )
 
 :: 4. Create Hubot file with a coffee extension
+echo copying hubot
 copy /Y "%DEPLOYMENT_TARGET%\node_modules\hubot\bin\hubot" "%DEPLOYMENT_TARGET%\node_modules\hubot\bin\hubot.coffee"
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
