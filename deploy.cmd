35cbc7050f3985313741f38f7a3b0af5f607a634
Isaac Cummings <Cummingsi1993@gmail.com>
2015-06-30 20:57:09 -0400

    continue with the diagnostics

--- deploy.cmd
+++ deploy.cmd
@@ -93,6 +93,7 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
+echo selecting node version
 
 :: 2. Select node version
 call :SelectNodeVersion
